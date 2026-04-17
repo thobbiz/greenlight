0c07df02e60f54c271239b24026d4473391cf6e5
thobbiz <odelolatojumi@gmail.com>
2026-04-17 14:54:57 +0100

    feat: - initial commit - Created Movie Handlers

---
+++ cmd/api/movies.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"greenlight.thobbiz.net/internal/data"
+)
+
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "create a new movie")
+}
+
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"horror", "thriller", "action", "anime"},
+		Version:   1,
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module greenlight.thobbiz.net
+
+go 1.25.5
+
+require github.com/julienschmidt/httprouter v1.3.0
