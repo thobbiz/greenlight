764f73127e9605e32e7083ca5a4deb31d8c5f8b6
thobbiz <odelolatojumi@gmail.com>
2026-05-25 21:10:05 +0100

    feat: added mock models

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -27,6 +27,24 @@ func (m MovieModel) Delete(id int64) error {
 	return nil
 }
 
+type MockMovieModel struct{}
+
+func (m MockMovieModel) Insert(movie *Movie) error {
+	return nil
+}
+
+func (m MockMovieModel) Get(id int64) (*Movie, error) {
+	return nil, nil
+}
+
+func (m MockMovieModel) Update(movie *Movie) error {
+	return nil
+}
+
+func (m MockMovieModel) Delete(id int64) error {
+	return nil
+}
+
 type Movie struct {
 	ID        int64     `json:"id"`
 	CreatedAt time.Time `json:"-"`
